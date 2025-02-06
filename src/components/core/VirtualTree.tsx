e0fec8495b4ad0047ee87275e4e4587259f6fb91
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-06 13:41:59 +0330

    chore: VirtualTree component margin top fixed

--- src/components/core/VirtualTree.tsx
+++ src/components/core/VirtualTree.tsx
@@ -6,7 +6,7 @@ export type TreeProps = { tree: TTree; rootAlias?: string };
 
 const VirtualTree = ({ tree, rootAlias = '.' }: TreeProps) => {
   return (
-    <pre className='directory-tree border border-neutral-400/10 bg-white dark:bg-[#0a0a0a]'>
+    <pre className='directory-tree !-mt-3 border border-neutral-400/10 bg-white dark:bg-[#0a0a0a]'>
       <code>{rootAlias + stringifyTree(tree.items)}</code>
     </pre>
   );
