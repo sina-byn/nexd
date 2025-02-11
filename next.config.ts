493cab7934c43503133b28e2179580d3826a00ea
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-11 09:21:57 +0330

    chore: md and mdx added to page extensions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   
   // Optionally, add any other Next.js config below
 };
