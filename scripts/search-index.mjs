6836b0642b624d107b9f2789561d2ef21257c3f3
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-12 15:56:37 +0330

    fix: search feature url bug fixed

--- scripts/search-index.mjs
+++ scripts/search-index.mjs
@@ -7,14 +7,15 @@ import * as pagefind from 'pagefind';
 import fastGlob from 'fast-glob';
 const { globSync: fg } = fastGlob;
 
-const pages = fg('./.next/server/app/**/*.html', { absolute: true, ignore: '**/_*.html' });
+const pages = fg('./.next/server/app/**/*.html', { ignore: '**/_*.html' });
 
 const { index } = await pagefind.createIndex();
 
 for (const page of pages) {
+  const pathname = page.replace('./.next/server/app', '').replace(/index.html|.html/g, '');
   const html = fs.readFileSync(page, 'utf-8');
 
-  const { errors } = index.addHTMLFile({ content: html, sourcePath: page });
+  const { errors } = index.addHTMLFile({ content: html, url: pathname });
   if (errors && errors.length > 0) console.error(errors);
 }
 
