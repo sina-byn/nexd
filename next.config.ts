82d4d406a41e1c5b0b44799f8a79aac84164bdc5
sina-byn <sina.sina.bayan@protonmail.com>
2025-08-15 18:50:47 +0330

    build: output field configured for static exports

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: { ignoreDuringBuilds: true },
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  
+
   // Optionally, add any other Next.js config below
 };
 
