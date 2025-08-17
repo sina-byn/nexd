191ee3927553458c85f78835ff53985d929c6198
sina-byn <sina.sina.bayan@protonmail.com>
2025-08-17 13:48:38 +0330

    fix: search-index script fixed to handle export mode as well

--- scripts/search-index.mjs
+++ scripts/search-index.mjs
@@ -1,5 +1,8 @@
 import fs from 'fs';
 
+// * config
+import nextConfig from '../next.config.mjs';
+
 // * pagefind
 import * as pagefind from 'pagefind';
 
@@ -7,12 +10,16 @@ import * as pagefind from 'pagefind';
 import fastGlob from 'fast-glob';
 const { globSync: fg } = fastGlob;
 
-const pages = fg('./.next/server/app/**/*.html', { ignore: '**/_*.html' });
+const isExportMode = nextConfig.output === 'export';
+const pattern = isExportMode ? './out/**/*.html' : './.next/server/app/**/*.html';
+const prefix = isExportMode ? './out' : './.next/server/app';
+
+const pages = fg(pattern, { ignore: '**/_*.html' });
 
 const { index } = await pagefind.createIndex();
 
 for (const page of pages) {
-  const pathname = page.replace('./.next/server/app', '').replace(/index.html|.html/g, '');
+  const pathname = page.replace(prefix, '').replace(/index.html|.html/g, '');
   const html = fs.readFileSync(page, 'utf-8');
 
   const { errors } = index.addHTMLFile({ content: html, url: pathname });
